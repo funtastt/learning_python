c881004cb99836106f624a515daf1cfbb54e4eff
Salavat <asnsalavat@gmail.com>
2021-11-22 18:46:51 +0100

    Weak typization in Python clearly

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# Короче, это как Map в Java
-# Ключ - какая-то строка String
-# Значение - что угодно (строка, число, объект, еще один словарь, список паролей Google...)
-dictionary = {
-    'name': "dict",
-    'value': 23324
-}
-dictionary2 = {
-    'name': "dict2",
-    'value': 5,
-    'refer': dictionary
-}
-print(dictionary2)
+# Статическая типизация - в Java, C++, C#  т.д.
+# Динамическая типизация - в JS, Python и т.д. - то, что нам нужно.
+def get_sum(a, b):
+    return a + b
+
+
+print(get_sum(23, 34))
+print(get_sum("23", "34"))
+print(get_sum([12, 23], [34, 54]))
+print(get_sum("23", 2)) # TypeError: can only concatenate str (not "int") to str
+
