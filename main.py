e675a29afb17f3c9e2ce5ab24bb2dcd844c0b976
Salavat <asnsalavat@gmail.com>
2021-11-22 18:06:03 +0100

    Dicks (dictionaries)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello world')
+# Короче, это как Map в Java
+# Ключ - какая-то строка String
+# Значение - что угодно (строка, число, объект, еще один словарь, список паролей Google...)
+dictionary = {
+    'name': "dict",
+    'value': 23324
+}
+dictionary2 = {
+    'name': "dict2",
+    'value': 5,
+    'refer': dictionary
+}
+print(dictionary2)
