628ffeaf3be2eb4586f20e73fac28d65a1ec390b
Salavat <asnsalavat@gmail.com>
2021-11-23 20:54:06 +0100

    Learning Python, and GitHub

--- main.py
+++
@@ -1,8 +0,0 @@
-from function import get_formatted_name
-
-first = input("Введите имя: ")
-
-last = input("Теперь фамилию: ")
-
-formatted_name = get_formatted_name(first, last)
-print("Вас зовут: " + formatted_name + '.')
