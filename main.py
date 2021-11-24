fb57dce9a7c16a20a398cbf4566b9cfcb85db6d3
Salavat <asnsalavat@gmail.com>
2021-11-24 20:38:04 +0100

    Posledovatelnost'

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-class Object:
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+# Распаковка последовательности
 
-    def __repr__(self):
-        return str(self.name) + ": " + str(self.value)
+massive = [1, 2, 3, 4, 5]
 
-
-# Список - массив ссылок на объекты
-test_list = [Object("Name", 32), 3405, "hello"]
-print(test_list)  # Выводит - [<__main__.Object object at 0x00000139E226FFD0>, 3405, 'hello']
-test_list = list(map(str, test_list))
-print(test_list) # Выводит - ['Name: 32', '3405', 'hello']
+a, *b = massive
+print(a)  # 1
+print(b)  # [2, 3, 4, 5]
