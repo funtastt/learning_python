d9e2936b27726249bad388f85190aa852504faca
Salavat <asnsalavat@gmail.com>
2021-11-24 20:44:09 +0100

    .

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 massive = [1, 2, 3, 4, 5]
 
-a, *b = massive
+a, b, *c = massive
+d, *e, f = massive
+*g, h, i = massive
 print(a)  # 1
-print(b)  # [2, 3, 4, 5]
+print(b)  # 2
+print(c)  # [3, 4, 5]
+print(d)  # 1
+print(e)  # [2, 3, 4]
+print(f)  # 5
+print(g)  # [1, 2, 3]
+print(h)  # 4
+print(i)  # 5
+
