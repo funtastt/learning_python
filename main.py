aba674e5ef763cddc82819dab3f19e7611097db3
Salavat <asnsalavat@gmail.com>
2021-11-25 21:22:47 +0100

    Public, Protected, Private

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# Распаковка последовательности
+import sys
+sys.setrecursionlimit(1000)
 
-massive = [1, 2, 3, 4, 5]
+class Object:
+    def __init__(self, a, b, c):
+        self.a = a  # Public
+        self._b = b  # Protected
+        self.__c = c  # Private
 
-a, b, *c = massive
-d, *e, f = massive
-*g, h, i = massive
-print(a)  # 1
-print(b)  # 2
-print(c)  # [3, 4, 5]
-print(d)  # 1
-print(e)  # [2, 3, 4]
-print(f)  # 5
-print(g)  # [1, 2, 3]
-print(h)  # 4
-print(i)  # 5
+    # Getter для protected
+    @property
+    def b(self):
+        return self._b
 
+    # Getter для private - ничем не отличается
+    @property
+    def c(self):
+        return self.__c
+
+
+obj = Object(1, 2, 3)
+print(obj.a)
+print(obj.b)
+print(obj.c)
